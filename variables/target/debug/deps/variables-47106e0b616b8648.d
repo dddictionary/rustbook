32a421eee5b5d2a91a5d308245a60f48bc42d0b6
Abrar <abrarhabib285@gamil.com>
2022-11-24 16:07:39 -0500

    added variables lesson

---
+++ variables/target/debug/deps/variables-47106e0b616b8648.d
@@ -0,0 +1,5 @@
+/home/abrar/rustprojects/variables/target/debug/deps/variables-47106e0b616b8648.rmeta: src/main.rs
+
+/home/abrar/rustprojects/variables/target/debug/deps/variables-47106e0b616b8648.d: src/main.rs
+
+src/main.rs:
